5d2f7a042a5103eac0fba2a11b55d11741a0f1ec
Andrew Brudnak <brudnak@icloud.com>
2019-07-15 09:52:26 -0700

    :bow_and_arrow: Getters & setters created

--- webemployees/src/main/java/com/lambdaschool/webemployees/Employee.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/Employee.java
@@ -24,4 +24,58 @@ public class Employee {
         this.companyID = companyID;
         this.healthPlanID = healthPlanID;
     }
+
+    // start of - GETTERS & SETTERS
+    public long getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public boolean isHas401K() {
+        return has401K;
+    }
+
+    public void setHas401K(boolean has401K) {
+        this.has401K = has401K;
+    }
+
+    public int getCompanyID() {
+        return companyID;
+    }
+
+    public void setCompanyID(int companyID) {
+        this.companyID = companyID;
+    }
+
+    public int getHealthPlanID() {
+        return healthPlanID;
+    }
+
+    public void setHealthPlanID(int healthPlanID) {
+        this.healthPlanID = healthPlanID;
+    }
+    // end of - GETTERS & SETTERS
 }
