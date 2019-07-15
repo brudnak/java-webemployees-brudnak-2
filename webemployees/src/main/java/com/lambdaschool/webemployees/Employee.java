04fd0b1ac8fcf0f09d40dd50e5addb72633ddbb6
Andrew Brudnak <brudnak@icloud.com>
2019-07-15 09:48:54 -0700

    :man_farmer: Employee fields created

--- webemployees/src/main/java/com/lambdaschool/webemployees/Employee.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/Employee.java
@@ -1,4 +1,16 @@
 package com.lambdaschool.webemployees;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Employee {
+
+    //fields
+    private static final AtomicLong counter = new AtomicLong();
+    private long id;
+    private String fname;
+    private String lname;
+    private double salary;
+    private boolean has401K;
+    private int companyID;
+    private int healthPlanID;
 }
