61081a8b119ad6d0c88e7125a2e3effd2c60de9d
Andrew Brudnak <brudnak@icloud.com>
2019-07-15 10:08:22 -0700

    :alien: Adding methods to EmpList class

--- webemployees/src/main/java/com/lambdaschool/webemployees/EmpList.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/EmpList.java
@@ -16,5 +16,33 @@ public class EmpList {
         empList.add(new Employee("John", "Jones ", 75000, true, 1, 1));
     }
 
+    public Employee findEmployee(CheckEmployee tester) {
+        for (Employee e: empList) {
+            if (tester.test(e)) {
+                return e;
+            }
+        }
+
+        return null;
+    }
+
+    public ArrayList<Employee> findEmployees(CheckEmployee tester) {
+
+        // creating TEMPORARY array list of type Employee
+        ArrayList<Employee> tempEmpList = new ArrayList<>();
+
+        // loop through employees in empList
+        for (Employee e: empList) {
+
+            // if LAMBDA EXPRESSION is TRUE
+            if (tester.test(e)) {
+
+                // ADD to the newly created TEMPORARY array list
+                tempEmpList.add(e);
+            }
+        }
+
+        return tempEmpList;
+    }
 }
 
