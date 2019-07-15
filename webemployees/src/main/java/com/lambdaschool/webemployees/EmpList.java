8aa94f26e4784f34cb83a5535ab1e9bba52e27b8
Andrew Brudnak <brudnak@icloud.com>
2019-07-15 10:00:49 -0700

    :smile: Created array list with data

--- webemployees/src/main/java/com/lambdaschool/webemployees/EmpList.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/EmpList.java
@@ -1,4 +1,20 @@
 package com.lambdaschool.webemployees;
 
+import java.util.ArrayList;
+
 public class EmpList {
+
+    // creating array list named empList
+    public ArrayList<Employee> empList = new ArrayList<>();
+
+    // constructor
+    public EmpList() {
+
+        // Initializing data onto the array list empList
+        empList.add(new Employee("Steve", "Green", 45000, true, 1, 1));
+        empList.add(new Employee("Sam", "Ford", 80000, true, 1, 1));
+        empList.add(new Employee("John", "Jones ", 75000, true, 1, 1));
+    }
+
 }
+
